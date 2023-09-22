5d02aa56ac7e2213cd5a113499d4d219c5a84efd
victormayowa <hardmanvicky01@gmail.com>
2023-09-22 05:17:37 -0700

    update user

--- models/user.py
+++ models/user.py
@@ -9,11 +9,13 @@ import os
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
+
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+
         places = relationship("Place", back_populates="user",
                               cascade="all, delete")
         reviews = relationship("Review", back_populates="user",
