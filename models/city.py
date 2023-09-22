a3feea49f00706038e77e05f49d84396c4cb72f0
victormayowa <hardmanvicky01@gmail.com>
2023-09-22 05:25:06 -0700

    udpdate user

--- models/city.py
+++ models/city.py
@@ -9,11 +9,16 @@ import os
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
+
     if os.getenv("HBNB_TYPE_STORAGE") == 'db':
         name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        state_id = Column(String(60),
+                          ForeignKey('states.id'),
+                          nullable=False)
+
         places = relationship("Place",
-                              back_populates="cities", cascade="all, delete")
+                              back_populates="cities",
+                              cascade="all, delete")
     else:
         state_id = ""
         name = ""
