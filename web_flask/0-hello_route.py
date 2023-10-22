d59f2876572e8e2fbc6a3136f309b9214978770b
victormayowa <hardmanvicky01@gmail.com>
2023-10-22 15:51:14 -0700

    FlaskWebFramework

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """A simple Flask web application"""
-
 from flask import Flask
+
+
+app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    '''returns hello'''
     return 'Hello HBNB!'
 
 
